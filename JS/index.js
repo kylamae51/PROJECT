2fa6048494569416d950fecdae633cff7a90ad1a
kylamae51 <kylavillafranca016@gmail.com>
2023-02-08 16:00:35 +0800

    first commit

---
+++ JS/index.js
@@ -0,0 +1,24 @@
+//var a="Let's have a cup of coffee.";
+//var b='He said "Hello" and left.';
+//var c='We\ll never give up.';
+
+//document.write(a + "<br>" + b + "<br>" + c);
+
+
+//var a =25;
+//var b =30.5;
+//var c = 4.25e+6;
+//var d = 4.25-6;
+
+//document.write(a + b + c +d);
+
+
+//var person = {"name: Kyla, surname: Villafranca, age: 22};
+//document.write(person);
+
+var total = function(){
+  var submit=  parseInt(document.getElementById("magic").value) + parseInt(document.getElementById("magic").value);
+    return ("the total is "+submit);
+}
+alert(total());
+
