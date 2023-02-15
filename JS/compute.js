1a2de501970c325cc258de293e983917e0362572
Eyman-Codes <emmandequillaqt08@gmail.com>
2023-02-15 15:48:55 +0800

    Added File

---
+++ JS/compute.js
@@ -0,0 +1,37 @@
+//function message(){
+
+//alert('You pressed a key inside textarea!');
+//}
+
+//function colorchange(elem){
+
+    //elem.style.backgroundColor= "blue";
+    //}
+
+
+
+    //function someFunction(){
+        
+        //alert('')
+    //}
+
+//<script>
+        //function displayWindowSize() {
+        //var w = window.outerWidth;
+       // var h = window.outerHeight;
+       // var txt = "Window size: width="+ w + ", height=" + h;
+       // document getElementById("result").innerHTML = txt;
+
+        //</script>
+
+
+        var now =new Date();
+        var dayOfWeek = now.getDay();
+        if(dayOfWeek ==5){
+
+            alert('Hello Wednesday!');
+    
+        }elseif (dayOfWeek ==0){
+
+
+        }
